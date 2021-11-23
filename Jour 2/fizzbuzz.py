1ab1e22470f14125d190591737b5319ffa79f133
Matthieu Falce <matthieu@falce.net>
2021-11-23 15:32:52 +0100

    gitautosync update 180

---
+++ Jour 2/fizzbuzz.py
@@ -0,0 +1,23 @@
+def est_divisible_par_3(nombre):
+    return nombre % 3 == 0
+
+def est_divisible_par_5(nombre):
+    return nombre % 5 == 0
+
+def regle_fizz_buzz(nombre):
+    resultat = ""
+    if est_divisible_par_3(nombre):
+        resultat = resultat + "fizz"
+    if est_divisible_par_5(nombre):
+        resultat += "buzz"
+    if not resultat :
+        resultat = nombre
+    return resultat
+        
+# for nombre in range(1, 101):
+#     print(regle_fizz_buzz(nombre))
+
+regle_fizz_buzz(3) == "Fizz"
+regle_fizz_buzz(5) == "Buzz"
+regle_fizz_buzz(15) == "FizzBuzz"
+regle_fizz_buzz(2) == 2
