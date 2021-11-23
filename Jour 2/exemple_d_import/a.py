7f2bfad07fd3ac4e35b712366c03bc1b2ddfbb03
Matthieu Falce <matthieu@falce.net>
2021-11-23 14:55:49 +0100

    gitautosync update 155

--- Jour 2/exemple_d_import/a.py
+++ Jour 2/exemple_d_import/a.py
@@ -1,13 +1,12 @@
-import b 
 
 print("je charge a")
 
 def randint(a, b):
+    import b 
     print("dans randint de a")
 
 def randrange(a, b):
     print("dans randint de a")
-
 
 
 print("fin du chargement de a")
