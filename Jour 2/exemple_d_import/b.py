6cd2a0322f49ec57b3bfe0927b56d44dcfa2e35f
Matthieu Falce <matthieu@falce.net>
2021-11-23 14:59:05 +0100

    gitautosync update 160

--- Jour 2/exemple_d_import/b.py
+++ Jour 2/exemple_d_import/b.py
@@ -2,6 +2,7 @@ import random as le_random_de_base
 from a import randint as le_randint_de_a
 from a import randint, randrange
 
-import numpy as np 
-import pandas as pd 
-import matplotlib.pyplot as plt 
+# import numpy as np 
+# import pandas as pd 
+# import matplotlib.pyplot as plt 
+
