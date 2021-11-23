8d023c5eb3ed1e4cffa265ed55fe0dc70465e8e9
Matthieu Falce <matthieu@falce.net>
2021-11-23 12:42:23 +0100

    gitautosync update 106

--- Jour 2/plus_ou_moins.py
+++ Jour 2/plus_ou_moins.py
@@ -1,3 +1,5 @@
+import random
+
 def my_input():
     """
     On demande un nombre à l'utilisateur tant que son entrée est invalide
@@ -30,23 +32,49 @@ def my_input_2():
         else:
             return res
 
-import random
+def soluution_1():
+    NOMBRE_A_TROUVER = random.randint(1, 100)
+    while (nombre_entre := my_input_2()) != NOMBRE_A_TROUVER:
+        if nombre_entre > NOMBRE_A_TROUVER:
+            print("c'est moins")
+        else:
+            print("c'est plus")
+    print("c'est fini")
 
+def solution_2():
+    while True:
+        if nombre_entre > NOMBRE_A_TROUVER:
+            print("c'est moins")
+        elif nombre_entre < NOMBRE_A_TROUVER:
+            print("c'est plus")
+        else:
+            break 
+        nombre_entre = my_input_2()
+    print("c'est fini")
 
-NOMBRE_A_TROUVER = random.randint(1, 100)
-while (nombre_entre := my_input_2()) != NOMBRE_A_TROUVER:
-    if nombre_entre > NOMBRE_A_TROUVER:
-        print("c'est moins")
-    else:
-        print("c'est plus")
-print("c'est fini")
+def solution_3_nicolas():
+    def input_int(question):
+        nb = input("Proposition ? ")
+        if nb.isnumeric():
+            return int(nb)
+        print("Mauvaise saisie")
+        return 0
 
-# while True:
-#     if nombre_entre > NOMBRE_A_TROUVER:
-#         print("c'est moins")
-#     elif nombre_entre < NOMBRE_A_TROUVER:
-#         print("c'est plus")
-#     else:
-#         break 
-#     nombre_entre = my_input_2()
-# print("c'est fini")
+    nombre_ordinateur = random.randint(1, 10)
+    print("J'ai choisi un nombre entre 1 et 10, devinez le ?")
+
+    nombre_utilisateur = 0
+
+    while nombre_utilisateur != nombre_ordinateur:
+        # Saisie
+        nombre_utilisateur = input_int("Proposition ? ")
+        if not nombre_utilisateur:
+            continue
+
+        # Vérification
+        if nombre_utilisateur < nombre_ordinateur:
+            print("C'est Plus")
+        if nombre_utilisateur > nombre_ordinateur:
+            print("C'est Moins")
+
+    print("Bravo, c'était bien ", nombre_ordinateur)
