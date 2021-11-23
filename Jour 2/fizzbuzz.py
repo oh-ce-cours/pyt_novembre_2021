5d789f52a87471083da9730c2ce2d57969c4228d
Matthieu Falce <matthieu@falce.net>
2021-11-23 15:33:35 +0100

    gitautosync update 181

--- Jour 2/fizzbuzz.py
+++ Jour 2/fizzbuzz.py
@@ -17,7 +17,7 @@ def regle_fizz_buzz(nombre):
 # for nombre in range(1, 101):
 #     print(regle_fizz_buzz(nombre))
 
-regle_fizz_buzz(3) == "Fizz"
-regle_fizz_buzz(5) == "Buzz"
-regle_fizz_buzz(15) == "FizzBuzz"
-regle_fizz_buzz(2) == 2
+print(regle_fizz_buzz(3) == "Fizz")
+print(regle_fizz_buzz(5) == "Buzz")
+print(regle_fizz_buzz(15) == "FizzBuzz")
+print(regle_fizz_buzz(2) == 2)
