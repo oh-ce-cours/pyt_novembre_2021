d6c5938e691e79bdbdb05768895ec12c697d7883
Matthieu Falce <matthieu@falce.net>
2021-11-23 12:13:12 +0100

    gitautosync update 72

--- Jour 2/plus_ou_moins.py
+++ Jour 2/plus_ou_moins.py
@@ -4,12 +4,12 @@ def my_input():
     return res 
 
 NOMBRE_A_TROUVER = 5
-nombre_entre = int(input("Veuillez entrer un nombre: "))
+nombre_entre = my_input()
 print(nombre_entre)
 while nombre_entre != NOMBRE_A_TROUVER:
     if nombre_entre > NOMBRE_A_TROUVER:
         print("c'est moins")
     else:
         print("c'est plus")
-    nombre_entre = int(input("Veuillez entrer un nombre: "))
+    nombre_entre = my_input()
 print("c'est fini")
