2bdf2fa532fdc86437ee302ce923f4800275bd92
Matthieu Falce <matthieu@falce.net>
2021-11-23 12:37:58 +0100

    gitautosync update 105

--- Jour 2/plus_ou_moins.py
+++ Jour 2/plus_ou_moins.py
@@ -34,14 +34,11 @@ import random
 
 
 NOMBRE_A_TROUVER = random.randint(1, 100)
-nombre_entre = 
-print(nombre_entre)
 while (nombre_entre := my_input_2()) != NOMBRE_A_TROUVER:
     if nombre_entre > NOMBRE_A_TROUVER:
         print("c'est moins")
     else:
         print("c'est plus")
-    nombre_entre = my_input_2()
 print("c'est fini")
 
 # while True:
