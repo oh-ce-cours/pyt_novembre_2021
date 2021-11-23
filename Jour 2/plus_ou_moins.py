5ae8681a6d342e374871ebb39a1aa7e41f7c19e1
Matthieu Falce <matthieu@falce.net>
2021-11-23 12:04:58 +0100

    gitautosync update 62

--- Jour 2/plus_ou_moins.py
+++ Jour 2/plus_ou_moins.py
@@ -1,4 +1,4 @@
 NOMBRE_A_TROUVER = 5
-nombre_entre = input("Veuillez entrer un nombre: ")
+nombre_entre = int(input("Veuillez entrer un nombre: "))
 print(nombre_entre)
 print(nombre_entre == NOMBRE_A_TROUVER)
