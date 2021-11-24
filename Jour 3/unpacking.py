bc82a7f150d2c838afce5916352293d2afa1f50c
Matthieu Falce <matthieu@falce.net>
2021-11-24 11:14:57 +0100

    gitautosync update 60

--- Jour 3/unpacking.py
+++ Jour 3/unpacking.py
@@ -7,6 +7,6 @@ l:List[Tuple[bool, str, type_coords]] = [
     (True, "super", (-1, 3, 5)),
 ]
 
-status, b, c = l[0]
+status, b, (x, y,z) = l[0]
 for status, _, (_, coord_y, _) in l:
     print(status, coord_y)
