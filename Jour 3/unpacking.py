e415493222aab4257fe0f2afdf44bd830e4d7c2d
Matthieu Falce <matthieu@falce.net>
2021-11-24 11:08:38 +0100

    gitautosync update 53

--- Jour 3/unpacking.py
+++ Jour 3/unpacking.py
@@ -1,9 +1,9 @@
 from typing import List, Any, Tuple
 
 l:List[Tuple[bool, str, Any]] = [
-    [True, "message", (1, 2, 3)],
-    [False, "casse", (1, 1, 1)],
-    [True, "super", (-1, 3, 5)],
+    (True, "message", (1, 2, 3)),
+    (False, "casse", (1, 1, 1)),
+    (True, "super", (-1, 3, 5)),
 ]
 
 status, _, (_, coord_y, _) = l[0]
