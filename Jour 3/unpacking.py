e5ba6af96b4886c7a2ea5c289ae220812183019a
Matthieu Falce <matthieu@falce.net>
2021-11-24 11:08:50 +0100

    gitautosync update 54

--- Jour 3/unpacking.py
+++ Jour 3/unpacking.py
@@ -1,6 +1,6 @@
 from typing import List, Any, Tuple
 
-l:List[Tuple[bool, str, Any]] = [
+l:List[Tuple[bool, str, Tuple[int, int, int]]] = [
     (True, "message", (1, 2, 3)),
     (False, "casse", (1, 1, 1)),
     (True, "super", (-1, 3, 5)),
