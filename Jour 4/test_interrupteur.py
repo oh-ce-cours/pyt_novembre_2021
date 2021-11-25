64b40dd86c89bd5d6047d01417f17fd80bf9dc37
Matthieu Falce <matthieu@falce.net>
2021-11-25 11:02:53 +0100

    gitautosync update 68

--- Jour 4/test_interrupteur.py
+++ Jour 4/test_interrupteur.py
@@ -6,3 +6,10 @@ def test_creation_interrupteur():
     assert i.etat == 0
     i.actionner_bouton(1)
     assert i.etat == 1
+
+
+def test_creation_interrupteur_2():
+    i = Interrupteur("chambre", 0)
+    assert i.etat == 0
+    i.actionner_bouton(1)
+    assert i.etat == 1
