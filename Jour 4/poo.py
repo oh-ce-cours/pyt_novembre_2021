0283c5a95925cd8295182cb72b5a8a8c374d74fa
Matthieu Falce <matthieu@falce.net>
2021-11-25 15:49:58 +0100

    gitautosync update 146

--- Jour 4/poo.py
+++ Jour 4/poo.py
@@ -3,7 +3,11 @@ class Personne:
         annee_en_cours = 2021
         self.naissance = naissance
         self.age = annee_en_cours - naissance
+        self.nom = nom
 
     def est_majeur(self):
         age_majorite = 18
         return self.age > age_majorite
+
+    def __str__(self):
+        return f"Je suis {} et je suis né en {self.naissance}"
