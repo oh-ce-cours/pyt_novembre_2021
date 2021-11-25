c42533ac7853b8de686e9a91a78b45b671c9ca08
Matthieu Falce <matthieu@falce.net>
2021-11-25 11:06:13 +0100

    gitautosync update 75

--- Jour 4/test_interrupteur.py
+++ Jour 4/test_interrupteur.py
@@ -6,12 +6,12 @@ def my_interrupteur():
 
 
 def test_creation_interrupteur(my_interrupteur):
-    assert i.etat == 0
-    i.actionner_bouton(1)
-    assert i.etat == 1
+    assert my_interrupteur.etat == 0
+    my_interrupteur.actionner_bouton(1)
+    assert my_interrupteur.etat == 1
 
 
 def test_creation_interrupteur_2(my_interrupteur):
-    assert i.etat == 1
-    i.actionner_bouton(10)
-    assert i.etat == 10
+    assert my_interrupteur.etat == 1
+    my_interrupteur.actionner_bouton(10)
+    assert my_interrupteur.etat == 10
