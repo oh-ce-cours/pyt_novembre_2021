78f170ffbb38d15371806d8435c569b336275b7f
Matthieu Falce <matthieu@falce.net>
2021-11-25 11:03:39 +0100

    gitautosync update 70

--- Jour 4/test_interrupteur.py
+++ Jour 4/test_interrupteur.py
@@ -1,15 +1,16 @@
 from interrupteur import Interrupteur
 
+i = Interrupteur("chambre", 0)
+
 
 def test_creation_interrupteur():
-    i = Interrupteur("chambre", 0)
     assert i.etat == 0
     i.actionner_bouton(1)
     assert i.etat == 1
 
 
 def test_creation_interrupteur_2():
-    i = Interrupteur("chambre", 1)
+    # i = Interrupteur("chambre", 1)
     assert i.etat == 1
     i.actionner_bouton(10)
     assert i.etat == 10
